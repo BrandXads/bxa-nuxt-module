2a7b91f5fe7f9bc2128b9ea0706fa063e6d44677
James Kehoe <james.m.kehoe@gmail.com>
2019-09-05 14:08:57 -0400

    dfgdgf

--- attribution.js
+++ attribution.js
@@ -1,17 +1,22 @@
-export const state = () => ({
+export default {
+  namespaced: true,
 
-})
-
-export const mutations = {
-  UPDATE_ATTRIBUTION (state, data) {
-    state[data.key] = data.value
-  }
-}
-
-export const actions = {
-  updateAttribution: ({ commit }) => {
-    for (let [key, value] of Object.entries(localStorage)) {
-      commit("UPDATE_ATTRIBUTION", { key, value })
+  state: () => ({
+    attribution: {}
+  }),
+  mutations: {
+    UPDATE_ATTRIBUTION (state, data) {
+      state.attribution[data.key] = data.value
     }
-  }
+  },
+
+  actions: {
+    updateAttribution: ({ commit }) => {
+      for (let [key, value] of Object.entries(localStorage)) {
+        commit("UPDATE_ATTRIBUTION", { key, value })
+      }
+    }
+  },
+
+  getters: {}
 }
