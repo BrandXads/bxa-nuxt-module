e9ed0dd2d45ecd5576e7f528b034b767cfdff420
James Kehoe <james.m.kehoe@gmail.com>
2019-09-05 13:19:42 -0400

    Remove reference to store

--- extract.js
+++ extract.js
@@ -1,4 +1,4 @@
-const extract = (store) => {
+const extract = () => {
   let url = new URL(window.location.search)
   let searchParams = new URLSearchParams(url)
 
@@ -6,7 +6,7 @@ const extract = (store) => {
   for (let pair of searchParams.entries()) {
     localStorage.setItem(pair[0], pair[1])
   }
-  store.dispatch('attribution/updateAttribution')
+  this.$store.dispatch('attribution/updateAttribution')
 }
 
 export default extract
