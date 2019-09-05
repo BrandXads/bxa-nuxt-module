69e2e1304e06668d5e189339f841198d01b9b979
James Kehoe <james.m.kehoe@gmail.com>
2019-09-05 13:30:10 -0400

    $

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import extract from './extract.js'
 const AffiliateLinkPlugin = {
   install(Vue) {
 
-    Vue.store.registerModule('attribution', attribution)
+    Vue.$store.registerModule('attribution', attribution)
 
     // Register components
     Vue.component('affiliate-link', AffiliateLink)
