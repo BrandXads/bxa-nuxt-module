d15adb0c91844d6f7ff049f8220464b9208a3469
James Kehoe <james.m.kehoe@gmail.com>
2019-09-09 16:13:32 -0400

    Change package name

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 // components/index.js
 import Vue from 'vue'
-import AffiliateLink from '../node_modules/bxa/components/lib/AffiliateLink.vue'
-import LinkWrap from '../node_modules/bxa/components/lib/LinkWrap.vue'
+import AffiliateLink from '../node_modules/bxa-nuxt-module/components/lib/AffiliateLink.vue'
+import LinkWrap from '../node_modules/bxa-nuxt-module/components/lib/LinkWrap.vue'
 
 // loop through components and register them
 Vue.component('affiliate-link', LinkWrap)
