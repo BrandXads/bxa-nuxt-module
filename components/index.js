4a1525dc55605829438193b256abebd280c1ef0a
James Kehoe <james.m.kehoe@gmail.com>
2019-09-09 14:40:54 -0400

    AHHHHH

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 // components/index.js
 import Vue from 'vue'
-import AffiliateLink from './lib/AffiliateLink.vue'
-import LinkWrap from './lib/LinkWrap.vue'
+import AffiliateLink from '../node_modules/bxa/components/lib/AffiliateLink.vue'
+import LinkWrap from '../node_modules/bxa/components/lib/LinkWrap.vue'
 
 // loop through components and register them
 Vue.component('affiliate-link', LinkWrap)
