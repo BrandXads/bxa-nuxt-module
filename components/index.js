e6f06cfedf76a904042dddaa4f59ecd56b834456
James Kehoe <james.m.kehoe@gmail.com>
2019-09-09 14:22:32 -0400

    index name

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 // components/index.js
 import Vue from 'vue'
-import components from './lib'
+import components from './lib/index.js'
 
 // loop through components and register them
 for (const name in components) {
