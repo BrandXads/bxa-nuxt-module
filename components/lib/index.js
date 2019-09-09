024d4e8289c29925e3991933b98f7f5f4c329918
James Kehoe <james.m.kehoe@gmail.com>
2019-09-09 14:35:06 -0400

    change import path

--- components/lib/index.js
+++ components/lib/index.js
@@ -1,5 +1,5 @@
-import AffiliateLink from './AffiliateLink.vue'
-import LinkWrap from './LinkWrap.vue'
+import AffiliateLink from '../node_modules/bxa/components/lib/AffiliateLink.vue'
+import LinkWrap from '../node_modules/bxa/components/lib/LinkWrap.vue'
 export default {
   AffiliateLink,
   LinkWrap
