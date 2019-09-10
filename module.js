73b3f36cef425dfab6d8d05fad7066b2aac8bcf5
James Kehoe <james.m.kehoe@gmail.com>
2019-09-10 09:29:27 -0400

    whoops

--- module.js
+++ module.js
@@ -4,8 +4,8 @@ export default function nuxtBootstrapVue (moduleOptions) {
   // Register `plugin.js` template
   this.addPlugin({
     src: path.resolve(__dirname, 'components/index.js'),
-    options: { 'template.ssr': false }
-})
+    ssr: false 
+  })
   this.addPlugin(path.resolve(__dirname, 'affiliate.js'))
 }
 module.exports.meta = require('./package.json')
